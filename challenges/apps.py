537849b7bc47cec8ee8ab433a299137343cb9634
EslamKamel89 <elsamkamelforex@gmail.com>
2025-10-28 04:36:17 +0300

    refactor(challenges): rename app from 'challlanges' to 'challenges' - Fix typo in app name across all files and Django config - Update VS Code interpreter path and import organization settings

--- challenges/apps.py
+++ challenges/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ChalllangesConfig(AppConfig):
+class ChallengesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'challlanges'
+    name = 'challenges'
